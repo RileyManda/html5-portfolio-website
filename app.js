b5ab6b3100c81d8d07a46c0330d1d40395ac849c
RileyManda <rileymanda69@gmail.com>
2023-05-22 19:00:27 +0200

    Removed decoration and underline from list items

--- app.js
+++ app.js
@@ -1,10 +1,10 @@
-const openNav = function openNavHandler () {
-    document.querySelector(".mobile-menu").style.display = "block";
-}
+const openNav = function openNavHandler() {
+  document.querySelector('.mobile-menu').style.display = 'block';
+};
 
-const closeNav = function closeNavHandler () {
-    document.querySelector(".mobile-menu").style.display = "none";
-}
+const closeNav = function closeNavHandler() {
+  document.querySelector('.mobile-menu').style.display = 'none';
+};
 
-document.querySelector("#open-nav").addEventListener('click', openNav);
-document.querySelector("#close-nav").addEventListener('click', closeNav);
+document.querySelector('#open-nav').addEventListener('click', openNav);
+document.querySelector('#close-nav').addEventListener('click', closeNav);
