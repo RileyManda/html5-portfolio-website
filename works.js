dddb241c974f2c4e019d36f34e32ab538049dcd0
RileyManda <rileymanda69@gmail.com>
2023-05-23 13:54:31 +0200

    Separation of concerns- works card

---
+++ works.js
@@ -0,0 +1,45 @@
+const workcards = [
+  {
+    title: 'Multi-Post Stories',
+    subtitle: 'Gain+Glory',
+    image: './assets/images/mobile/img-placeholder.png',
+    tags: ['Ruby on Rails', 'CSS', 'JavaScript', 'HTML'],
+    projectLink: '',
+  },
+  {
+    title: 'Multi-Post Stories',
+    subtitle: 'Gain+Glory',
+    image: 'path/to/image2.jpg',
+    tags: ['Ruby on Rails', 'CSS', 'JavaScript', 'HTML'],
+    projectLink: '',
+  },
+  {
+    title: 'Multi-Post Stories',
+    subtitle: 'Gain+Glory',
+    image: 'path/to/image2.jpg',
+    tags: ['Ruby on Rails', 'CSS', 'JavaScript', 'HTML'],
+    projectLink: '',
+  },
+  {
+    title: 'Multi-Post Stories',
+    subtitle: 'Gain+Glory',
+    image: 'path/to/image2.jpg',
+    tags: ['Ruby on Rails', 'CSS', 'JavaScript', 'HTML'],
+    projectLink: '',
+  },
+  {
+    title: 'Multi-Post Stories',
+    subtitle: 'Gain+Glory',
+    image: 'path/to/image2.jpg',
+    tags: ['Ruby on Rails', 'CSS', 'JavaScript', 'HTML'],
+    projectLink: '',
+  },
+  {
+    title: 'Multi-Post Stories',
+    subtitle: 'Gain+Glory',
+    image: 'path/to/image2.jpg',
+    tags: ['Ruby on Rails', 'CSS', 'JavaScript', 'HTML'],
+    projectLink: '',
+  },
+];
+export default workcards;
