55c19d522e32e4ebe2fd348e3d843ed0cc420998
RileyManda <rileymanda69@gmail.com>
2023-05-25 13:06:11 +0200

    Updated dynamic js with two buttons

--- detail-view.js
+++ detail-view.js
@@ -40,20 +40,32 @@ const generateDetailView = (data) => {
   summary.classList.add('detail-summary');
   summary.textContent = data.summary;
 
-  const sourceButton = document.createElement('button');
-  sourceButton.classList.add('detail-btn');
-  sourceButton.textContent = 'See Source';
-  const sourceLink = document.createElement('a');
-  sourceLink.href = data.sourceLink;
-  const sourceIcon = document.createElement('img');
-  sourceIcon.src = './assets/icons/github-light.svg';
-  sourceIcon.alt = 'GitHub icon';
-  sourceLink.appendChild(sourceIcon);
-  sourceButton.appendChild(sourceLink);
+  const sourceButton1 = document.createElement('button');
+  sourceButton1.classList.add('detail-btn');
+  sourceButton1.textContent = 'See Live';
+  const sourceLink1 = document.createElement('a');
+  sourceLink1.href = data.sourceLink;
+  const sourceIcon1 = document.createElement('img');
+  sourceIcon1.src = './assets/icons/live-icon.svg';
+  sourceIcon1.alt = 'Live icon';
+  sourceLink1.appendChild(sourceIcon1);
+  sourceButton1.appendChild(sourceLink1);
+
+  const sourceButton2 = document.createElement('button');
+  sourceButton2.classList.add('detail-btn');
+  sourceButton2.textContent = 'See Source';
+  const sourceLink2 = document.createElement('a');
+  sourceLink2.href = data.sourceLink;
+  const sourceIcon2 = document.createElement('img');
+  sourceIcon2.src = './assets/icons/github-light.svg';
+  sourceIcon2.alt = 'GitHub icon';
+  sourceLink2.appendChild(sourceIcon2);
+  sourceButton2.appendChild(sourceLink2);
 
   const buttonContainer = document.createElement('div');
   buttonContainer.classList.add('detail-btn-container');
-  buttonContainer.appendChild(sourceButton);
+  buttonContainer.appendChild(sourceButton1);
+  buttonContainer.appendChild(sourceButton2);
 
   detailViewContainer.appendChild(closeButton);
   detailViewContainer.appendChild(imageContainer);
