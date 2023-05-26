32d54b5fa68201a36e40a61c4302c78123c719b1
Riley Manda <rileymanda69@gmail.com>
2023-05-26 04:18:22 +0200

    Revert "Detail section"

--- app.js
+++ app.js
@@ -4,16 +4,11 @@ const openNav = function openNavHandler() {
 
 const closeView = function closeNavHandler() {
   document.querySelector('.mobile-menu').style.display = 'none';
+  document.querySelector('.detail-view').style.display = 'none';
 };
 
 const openDetailView = function openDetailHandler() {
-  const detailView = document.querySelector('.detail-view');
-  const blurOverlay = document.createElement('div');
-  blurOverlay.classList.add('blur-overlay');
-  document.body.appendChild(blurOverlay);
-
-  detailView.style.display = 'block';
-  document.body.classList.add('modal-open');
+  document.querySelector('.detail-view').style.display = 'block';
 };
 
 document.querySelector('#open-nav').addEventListener('click', openNav);
--- data.js
+++ data.js
@@ -59,40 +59,4 @@ export const aboutcards = [
       'API Design',
     ],
   },
-  {
-    html: `
-      <div class="about-title">
-        <h2>About Me</h2>
-        <p class="supporting-text">
-          I can help you build a product,<br> feature or website. Look through some of my work and
-          experience!<br>
-          If you like what you see and have <br>a project you need coded, <span>don't hesitate to contact
-          me.</span>
-        </p>
-        <button id="project-btn" class="project-btn">Get my resume</button>
-      </div>
-    `,
-  },
-];
-
-export const detailscontent = [
-  {
-    title: 'Keeping track of hundreds of components',
-    tags: [
-      'Codekit',
-      'GitHub',
-      'JavaScript',
-      'Bootstrap',
-      'Terminal',
-      'Codepen',
-      'Ruby on Rails',
-      'CSS',
-      'Javascript',
-    ],
-    summary: `Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s.<br><br>
-      Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s.`,
-    liveLink: 'https://rileymanda.github.io/portfolio-website/',
-    sourceLink: 'https://github.com/RileyManda/portfolio-website',
-    image: './assets/images/desktop/detail-view-desktop.png',
-  },
 ];
