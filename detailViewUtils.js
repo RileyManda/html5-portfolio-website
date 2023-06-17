532f2eb11fb68a1be285174c589af580c649f9ae
RileyManda <rileymanda69@gmail.com>
2023-06-17 17:30:58 +0200

    Removed empty strings and modularized the code

---
+++ detailViewUtils.js
@@ -0,0 +1,7 @@
+import { detailscontent } from './data.js';
+
+export const openDetailView = (index) => {
+  const card = document.getElementById(`card-${index}`);
+  const detailViewImage = detailscontent[index].detailimage;
+  card.style.backgroundImage = `url('${detailViewImage}')`;
+};
