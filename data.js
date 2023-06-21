4346bf70b101a9807567eb8c003c53df74398235
RileyManda <rileymanda69@gmail.com>
2023-06-21 22:20:48 +0200

    Implemented conference websit project to portfolio

--- data.js
+++ data.js
@@ -1,39 +1,39 @@
 export const workcards = [
   {
-    title: 'Website Portfolio',
+    title: 'Portfolio Website',
     subtitle: 'Mobile+Web',
     tags: ['CSS', 'JavaScript', 'HTML'],
-    image: './assets/images/projects/portfolio-website-desktop.png'
+    image: './assets/images/projects/portfolio-website-desktop.png',
+  },
+  {
+    title: 'Conference Website',
+    subtitle: 'Mobile+Web',
+    tags: ['CSS', 'JavaScript', 'HTML'],
+    image: './assets/images/projects/conference-website-project.png',
   },
   {
     title: 'Multi-Post Stories',
     subtitle: 'Gain+Glory',
     tags: ['Ruby on Rails', 'CSS', 'JavaScript', 'HTML'],
-    image: './assets/images/mobile/img-placeholder.png'
+    image: './assets/images/mobile/img-placeholder.png',
   },
   {
     title: 'Multi-Post Stories',
     subtitle: 'Gain+Glory',
     tags: ['Ruby on Rails', 'CSS', 'JavaScript', 'HTML'],
-    image: './assets/images/mobile/img-placeholder.png'
+    image: './assets/images/mobile/img-placeholder.png',
   },
   {
     title: 'Multi-Post Stories',
     subtitle: 'Gain+Glory',
     tags: ['Ruby on Rails', 'CSS', 'JavaScript', 'HTML'],
-    image: './assets/images/mobile/img-placeholder.png'
+    image: './assets/images/mobile/img-placeholder.png',
   },
   {
     title: 'Multi-Post Stories',
     subtitle: 'Gain+Glory',
     tags: ['Ruby on Rails', 'CSS', 'JavaScript', 'HTML'],
-    image: './assets/images/mobile/img-placeholder.png'
-  },
-  {
-    title: 'Multi-Post Stories',
-    subtitle: 'Gain+Glory',
-    tags: ['Ruby on Rails', 'CSS', 'JavaScript', 'HTML'],
-    image: './assets/images/mobile/img-placeholder.png'
+    image: './assets/images/mobile/img-placeholder.png',
   },
 ];
 
@@ -81,6 +81,27 @@ export const detailscontent = [
       "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the releaLorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the relea",
     liveLink: 'https://rileymanda.github.io/portfolio-website/',
     sourceLink: 'https://github.com/RileyManda/portfolio-website',
-    detailimage: './assets/images/projects/portfolio-website-desktop.png',
+    detailimage: './assets/images/projects/portfolio-website-card.png',
+  },
+  {
+    title: 'Keeping track of hundreds of components',
+    tags: [
+      'Codekit',
+      'GitHub',
+      'JavaScript',
+      'Bootstrap',
+      'Terminal',
+      'Codepen',
+      'Ruby on Rails',
+      'CSS',
+      'Javascript',
+    ],
+    summary: `Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s.<br><br>
+      Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s.`,
+    desktopsummary:
+      "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the releaLorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the relea",
+    liveLink: 'https://rileymanda.github.io/portfolio-website/',
+    sourceLink: 'https://github.com/RileyManda/portfolio-website',
+    detailimage: '/assets/images/projects/conference-website-project.png',
   },
 ];
