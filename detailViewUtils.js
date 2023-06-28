236c3ad25aea26392a90281368243616d78ae027
RileyManda <rileymanda69@gmail.com>
2023-06-28 21:09:36 +0200

    Removed unnecesary code

--- detailViewUtils.js
+++
@@ -1,7 +0,0 @@
-import { detailscontent } from './data.js';
-
-export const openDetailView = (detailscontent,index) => {
-  const card = document.getElementById(`card-${index}`);
-  const detailViewImage = detailscontent[index].detailimage;
-  card.style.backgroundImage = `url('${detailViewImage}')`;
-};
